314386d64854c8accc25314b44c32a1e56a47ad8
Stephany Silva <stephanyggoncalvess@gmail.com>
2026-09-10 11:42:27 -0300

    Update script.js

--- script.js
+++ script.js
@@ -96,3 +96,91 @@ document.addEventListener("DOMContentLoaded", function () {
     }
 
 });
+/* =====================================
+   ANATOMIA INTERATIVA
+===================================== */
+
+const pontosAnatomia =
+    document.querySelectorAll(".ponto-anatomia");
+
+const tituloParte =
+    document.querySelector("#tituloParte");
+
+const descricaoParte =
+    document.querySelector("#descricaoParte");
+
+const curiosidadeParte =
+    document.querySelector("#curiosidadeParte");
+
+
+const informacoesAnatomia = {
+
+    bracos: {
+        titulo: "Braços",
+
+        descricao:
+            "Os braços partem do disco central. Muitas estrelas-do-mar possuem cinco braços, mas algumas espécies apresentam dezenas deles.",
+
+        curiosidade:
+            "Os braços possuem estruturas importantes para locomoção, percepção e alimentação."
+    },
+
+
+    madreporito: {
+        titulo: "Madreporito",
+
+        descricao:
+            "O madreporito é uma estrutura localizada na superfície aboral e está conectado ao sistema hidrovascular da estrela-do-mar.",
+
+        curiosidade:
+            "Ele participa do sistema pelo qual a água circula internamente no animal."
+    },
+
+
+    boca: {
+        titulo: "Boca",
+
+        descricao:
+            "A boca está localizada na superfície inferior do disco central da estrela-do-mar.",
+
+        curiosidade:
+            "Algumas espécies conseguem exteriorizar parte do estômago durante a alimentação."
+    },
+
+
+    pes: {
+        titulo: "Pés ambulacrais",
+
+        descricao:
+            "Os pés ambulacrais são pequenas estruturas associadas ao sistema hidrovascular.",
+
+        curiosidade:
+            "Eles ajudam na locomoção, fixação, manipulação de alimento e percepção do ambiente."
+    }
+
+};
+
+
+pontosAnatomia.forEach(function (ponto) {
+
+    ponto.addEventListener("click", function () {
+
+        const parte =
+            ponto.dataset.parte;
+
+        const informacao =
+            informacoesAnatomia[parte];
+
+
+        tituloParte.textContent =
+            informacao.titulo;
+
+        descricaoParte.textContent =
+            informacao.descricao;
+
+        curiosidadeParte.textContent =
+            informacao.curiosidade;
+
+    });
+
+});
